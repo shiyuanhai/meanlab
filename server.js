0d68875976716e53cae2b3f12f8b119ee9f3232a
Yuanhai Shi <shiyuanhai@gmail.com>
2016-03-20 15:53:28 -0400

    Added implementation for express examples.
    Involving horizontal folder structure, static file host, ejs view and
    session.

--- server.js
+++ server.js
@@ -1,14 +1,9 @@
-/*
-	express
-*/
-var express = require('express');
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+
+var express = require('./config/express.js');
+
 var app = express();
-
-app.use('/', function(req, res){
-	res.send('Hello World.');
-});
-
 app.listen(3000);
 console.log('Server running at localhost:3000');
-
+ 
 module.exports = app;
